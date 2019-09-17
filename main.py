7edcc5da55af2284820252d7af2e8d60d677fe7e
Xiaoquan Kong <u1mail2me@gmail.com>
2019-09-17 17:40:30 +0800

    Update: using new API

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import collections
 import pickle
 import sys
 
-from tokenizer_tools.conllz.iterator_reader import conllx_iterator_reader
-from tokenizer_tools.tagset.NER.BILUO import BILUOSequenceEncoderDecoder
+from tokenizer_tools.tagset.offset.corpus import Corpus
 
 from seq2annotation.server.http import load_predict_fn
 from tokenizer_tools.tagset.offset.sequence import Sequence
@@ -38,35 +37,9 @@ def evaluate_offline(model, corpus):
     return sample_total, sample_right, span_total, span_right
 
 
-def read_corpus(data_file):
-    corpus = []
-    encoder = BILUOSequenceEncoderDecoder()
-    for conll_data in conllx_iterator_reader([data_file]):
-        char_list = conll_data.word_lines
-        tag_list = conll_data.attribute_lines[0]
-        # for line in conll_data:
-        #     char_list.append(line[0])
-        #     tag_list.append(line[1])
-
-        text = ''.join(char_list)
-
-        try:
-            seq = encoder.to_offset(tag_list, text)
-        except ValueError:
-            # print(conll_data)
-            # print(text)
-            # print(tag_list)
-            # print(conll_data.id)
-            raise
-
-        corpus.append(seq)
-
-    return corpus
-
-
 if __name__ == "__main__":
     server = load_predict_fn(sys.argv[1])
-    corpus = read_corpus(sys.argv[2])
+    corpus = Corpus.read_from_file(sys.argv[2])
 
     sample_total, sample_right, span_total, span_right = evaluate_offline(server.infer, corpus)
     sample_correct_rate = len(sample_right) / len(sample_total)
